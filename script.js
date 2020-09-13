30567ed87836722b2fc963e44b20b61619189251
Reinis Muiznieks <reinis.muiznieks1@gmail.com>
2020-09-13 20:06:32 +0300

    button design and viewport size

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let canvas = document.querySelector('.canvas');
-let n=200;
+let n
+
+function createGrid(){
 for(let i=0;i<n*n;i++){
     let pixel = document.createElement('div')
     canvas.appendChild(pixel);
@@ -8,12 +10,33 @@ for(let i=0;i<n*n;i++){
     pixel.style.width = `${100/n}%`
 
 }
-let pixels = document.querySelectorAll('.pixel');
-pixels.forEach(px => {px.addEventListener('mouseover', function(e){
-px.classList.add('touched')
-})
-    
-});
+}
+function setListeners(){
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(px => {px.addEventListener('mouseover', function(e){
+    px.classList.add('touched')
+    })  
+    });
+    }
+
+function clearCanvas(){
+    let clear = document.querySelectorAll('.touched')
+    clear.forEach(cl => cl.classList.remove('touched'));
+
+}
+function deleteGrid(){
+    let del = document.querySelectorAll('.pixel');
+    del.forEach(dl => dl.remove())
+}
+function start(){
+deleteGrid();
+n = document.forms["grid"]["ngrid"].value;
+console.log(n)
+createGrid();
+setListeners();
+
+
+}
 
 
 
